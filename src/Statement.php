d1955ff9677eb04e6c469f0e7b18371a3f754079
Klisóstom <klis@pm.me>
2021-03-20 10:59:05 -0300

    fix: Renomeando o parâmetro $perf para $aPerformance, no amountFor()

--- src/Statement.php
+++ src/Statement.php
@@ -19,23 +19,23 @@ class Statement
         );
     }
 
-    public function amountFor($perf, $play)
+    public function amountFor($aPerformance, $play)
     {
         $result = 0;
 
         switch ($play['type']) {
             case 'tragedy':
                 $result = 40000;
-                if ($perf['audience'] > 30) {
-                    $result += 1000 * ($perf['audience'] - 30);
+                if ($aPerformance['audience'] > 30) {
+                    $result += 1000 * ($aPerformance['audience'] - 30);
                 }
                 break;
             case 'comedy':
                 $result = 30000;
-                if ($perf['audience'] > 20) {
-                    $result += 10000 + 500 * ($perf['audience'] - 20);
+                if ($aPerformance['audience'] > 20) {
+                    $result += 10000 + 500 * ($aPerformance['audience'] - 20);
                 }
-                $result += 300 * $perf['audience'];
+                $result += 300 * $aPerformance['audience'];
                 break;
             default:
                 throw new Exception('Unknow type: ' . $play['type'], 1);
