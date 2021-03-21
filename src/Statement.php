40eeec559b847253ed8ce685a37cbcd101103462
Klisóstom <klis@pm.me>
2021-03-21 17:29:28 -0300

    fix: Mudar declaração de função

--- src/Statement.php
+++ src/Statement.php
@@ -7,9 +7,9 @@ use \NumberFormatter;
 
 class Statement
 {
-    private $numberFormatter;
 
     public function __construct(
+        private $numberFormatter = null,
         public int $totalAmount = 0,
         public int $volumeCredits = 0,
         public string $result = '',
@@ -20,41 +20,41 @@ class Statement
         );
     }
 
-    public function amountFor($aPerformance, $play)
-    {
-        $result = 0;
-
-        switch ($play['type']) {
-            case 'tragedy':
-                $result = 40000;
-                if ($aPerformance['audience'] > 30) {
-                    $result += 1000 * ($aPerformance['audience'] - 30);
-                }
-                break;
-            case 'comedy':
-                $result = 30000;
-                if ($aPerformance['audience'] > 20) {
-                    $result += 10000 + 500 * ($aPerformance['audience'] - 20);
-                }
-                $result += 300 * $aPerformance['audience'];
-                break;
-            default:
-                throw new Exception('Unknow type: ' . $play['type'], 1);
-        };
-
-        return $result;
-    }
-
     public function statement(array $invoice, array $plays): string
     {
         $playFor = function ($aPerformance) use ($plays) {
             return $plays[$aPerformance['playID']];
         };
 
+        $amountFor = function ($aPerformance, $play) use ($playFor)
+        {
+            $result = 0;
+
+            switch ($playFor($aPerformance)['type']) {
+                case 'tragedy':
+                    $result = 40000;
+                    if ($aPerformance['audience'] > 30) {
+                        $result += 1000 * ($aPerformance['audience'] - 30);
+                    }
+                    break;
+                case 'comedy':
+                    $result = 30000;
+                    if ($aPerformance['audience'] > 20) {
+                        $result += 10000 + 500 * ($aPerformance['audience'] - 20);
+                    }
+                    $result += 300 * $aPerformance['audience'];
+                    break;
+                default:
+                    throw new Exception('Unknow type: ' . $playFor($aPerformance)['type'], 1);
+            };
+
+            return $result;
+        };
+
         $this->result = "\nStatement for ".$invoice['customer']."\n";
 
         foreach ($invoice['performances'] as $perf) {
-            $thisAmount = $this->amountFor($perf, $playFor($perf));
+            $thisAmount = $amountFor($perf, $playFor($perf));
 
             // soma créditos por volume
             $this->volumeCredits += max($perf['audience'] - 30, 0);
