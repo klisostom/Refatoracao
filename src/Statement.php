79b9c02b05222ad3727905b2f03de93485ec8b4e
Klisóstom <klis@pm.me>
2021-03-22 13:59:41 -0300

    fix: htmlStatement, renderHtml

--- src/Statement.php
+++ src/Statement.php
@@ -16,6 +16,16 @@ class Statement
         $this->statementData = new CreateStatementData();
     }
 
+    public function usd($aNumber)
+    {
+        $result = new NumberFormatter(
+            'en_US',
+            NumberFormatter::CURRENCY,
+        );
+
+        return $result->format($aNumber / 100);
+    }
+
     public function statement(array $invoice, array $plays)
     {
         return $this->renderPlainText($this->statementData->createStatementData($invoice, $plays));
@@ -23,26 +33,39 @@ class Statement
 
     public function renderPlainText(array $data): string
     {
-        $usd = function ($aNumber) {
-            $result = new NumberFormatter(
-                'en_US',
-                NumberFormatter::CURRENCY,
-            );
-
-            return $result->format($aNumber / 100);
-        };
-
         $result = "\nStatement for ".$data['customer']."\n";
 
         foreach ($data['performances'] as $perf) {
             // exibe a linha para esta requisição
             $result .= "    " .
                 $perf['play']['name'].": " .
-                $usd($perf['amount']) .
+                $this->usd($perf['amount']) .
                 " (" . $perf['audience'] . " seats)\n";
         }
 
-        $result .= "Amount owed is " . $usd($data['totalAmount']) . "\n";
+        $result .= "Amount owed is " . $this->usd($data['totalAmount']) . "\n";
+        $result .= "You earned " . $data['totalVolumeCredits'] . " credits\n";
+        return $result;
+    }
+
+    public function htmlStatement(array $invoice, array $plays)
+    {
+        return $this->renderHtml($this->statementData->createStatementData($invoice, $plays));
+    }
+
+    public function renderHtml(array $data)
+    {
+        $result = "<h1>Statement for " . $data['customer'] . "</h1><br>";
+        $result .= "<table><br>";
+        $result .= "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+
+        foreach ($data['performances'] as $perf) {
+            $result .= " <tr><td>" . $perf['play']['name'] . "</td><td>" . $perf['audience'] . "</td>";
+            $result .= "<td>" . $this->usd($perf['amount']) . "</td></tr><br>";
+        }
+
+        $result .= "</table>";
+        $result .= "Amount owed is " . $this->usd($data['totalAmount']) . "\n";
         $result .= "You earned " . $data['totalVolumeCredits'] . " credits\n";
         return $result;
     }
