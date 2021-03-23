6f1e646f21356ff9d50a08b261698f928bd937b8
Klisóstom <klis@pm.me>
2021-03-22 21:09:57 -0300

    fix: getAmount() em ComedyCalculator class

--- src/Calculator/ComedyCalculator.php
+++ src/Calculator/ComedyCalculator.php
@@ -4,5 +4,19 @@ namespace App\Calculator;
 
 class ComedyCalculator extends PerformanceCalculator
 {
+    public function getAmount()
+    {
+        $result = 30000;
 
+        if ($this->aPerformance['audience'] > 20) {
+            $result += 10000 + 500 * ($this->aPerformance['audience'] - 20);
+        }
+
+        $result += 300 * $this->aPerformance['audience'];
+        return $result;
+    }
+
+    public function getVolumeCredits() {
+        return parent::getVolumeCredits() + floor($this->aPerformance['audience'] / 5);
+    }
 }
