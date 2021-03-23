273f38b501208c31ddb5fba66e51263482b70420
Klisóstom <klis@pm.me>
2021-03-22 21:10:33 -0300

    fix: modificações PerformanceCalculator class

--- src/Calculator/PerformanceCalculator.php
+++ src/Calculator/PerformanceCalculator.php
@@ -2,6 +2,7 @@
 
 namespace App\Calculator;
 
+use Error;
 use Exception;
 
 class PerformanceCalculator
@@ -20,38 +21,11 @@ class PerformanceCalculator
 
     public function getAmount()
     {
-        $result = 0;
-
-        switch ($this->aPlay['type']) {
-            case 'tragedy':
-                $result = 40000;
-                if ($this->aPerformance['audience'] > 30) {
-                    $result += 1000 * ($this->aPerformance['audience'] - 30);
-                }
-                break;
-            case 'comedy':
-                $result = 30000;
-                if ($this->aPerformance['audience'] > 20) {
-                    $result += 10000 + 500 * ($this->aPerformance['audience'] - 20);
-                }
-                $result += 300 * $this->aPerformance['audience'];
-                break;
-            default:
-                throw new Exception('Unknow type: ' . $this->aPlay['type'], 1);
-        };
-
-        return $result;
+        throw new Error('Subclass responsibility');
     }
 
     public function getVolumeCredits()
     {
-        $result = 0;
-        $result += max($this->aPerformance['audience'] - 30, 0);
-
-        if ('comedy' === $this->aPlay['type']) {
-            $result += floor($this->aPerformance['audience'] / 5);
-        }
-
-        return $result;
+        return max($this->aPerformance['audience'] - 30, 0);
     }
 }
